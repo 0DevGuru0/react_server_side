3bba140a1fe71b52c4ddc31b71643de1b66072e1
sajjadhadafi <af.hadfi@gmail.com>
2019-04-09 12:43:58 +0430

    fix adminlist show issue

--- client/hoc/requireAuth.js
+++ client/hoc/requireAuth.js
@@ -1,9 +1,11 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import {graphql} from 'react-apollo';
+import query from '../Graphql/query/user'
 export default ChildComponent=>{
     class requireAuth extends Component{
         render(){
-            switch (this.props.auth) {
+            switch (this.props.auth || this.props.data.user) {
                 case false:
                     return <div>you Should logIn</div>
                 default:
@@ -12,5 +14,7 @@ export default ChildComponent=>{
         }
     }
     const mapStateToProp = ({auth})=>({auth:auth.user})
-    return connect(mapStateToProp,null)(requireAuth)
+    return connect(mapStateToProp,null)(
+        graphql(query)(requireAuth)
+    )
 }
